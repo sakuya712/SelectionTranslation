3a3c640710f7467c6d420d3af5796c6b233087a7
sakuya <dummy@dummy>
2020-10-04 14:34:27 +0900

    add main Process

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,79 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "selection-translation" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('selection-translation.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from selection-translation!');
+	//My language => Translation language
+	let command1 = vscode.commands.registerCommand('selection-translation.fromMyToTranslation', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor == undefined){return undefined}
+		const sourceLangString = vscode.workspace.getConfiguration("selection-translation").get("My language");
+		const translationLang = vscode.workspace.getConfiguration("selection-translation").get("Translation language");
+		let gasTranslation = new GasTranslation(editor, sourceLangString, translationLang)
+		gasTranslation.ReplaceTranslation();
 	});
+	//Translation language => My language
+	let command2 = vscode.commands.registerCommand('selection-translation.fromTranslationToMy', () => {
+		const editor = vscode.window.activeTextEditor;
+		if (editor == undefined){return undefined}
+		const sourceLangString = vscode.workspace.getConfiguration("selection-translation").get("Translation language");
+		const translationLang = vscode.workspace.getConfiguration("selection-translation").get("My language");
+		let gasTranslation = new GasTranslation(editor, sourceLangString, translationLang)
+		gasTranslation.ReplaceTranslation();
 
-	context.subscriptions.push(disposable);
+	});
+	context.subscriptions.push(command1);
+	context.subscriptions.push(command2);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+class GasTranslation{
+	private editor: vscode.TextEditor;
+	private selectRange: vscode.Range;
+	private selectString: string;
+	private sourceLanguage: string;
+	private translationLanguage: string;
+	constructor(editor:vscode.TextEditor, sourceLang: unknown, translationLang: unknown) {
+		this.editor = editor;
+		this.selectRange = new vscode.Range(this.editor.selection.start, this.editor.selection.end);
+		if (typeof sourceLang === 'string') {
+			this.sourceLanguage = sourceLang;
+		} else {
+			this.sourceLanguage = "";
+		}
+		if (typeof translationLang === 'string') {
+			this.translationLanguage = translationLang;
+		} else {
+			this.translationLanguage = "";
+		}
+		//Examine the current selected character
+		const startSelectLine: number = this.selectRange.start.line;
+		const EndSelectLine: number = this.selectRange.end.line;
+		if (startSelectLine == EndSelectLine) {
+			this.selectString = this.editor.document.lineAt(startSelectLine).text.slice(this.selectRange.start.character, this.selectRange.end.character);
+		} else {
+			this.selectString = "";
+			// Note: I want to support multiple lines
+			// selectString = editor.document.lineAt(startSelectLine).text.slice(selectRange.start.character) 
+			// for (let line = startSelectLine + 1; line < EndSelectLine; line++) {
+			// 	selectString = selectString + editor.document.lineAt(line).text;
+			// }
+			// selectString = selectString + editor.document.lineAt(startSelectLine).text.slice(0,selectRange.end.character);
+		}
+	}
+	ReplaceTranslation() {
+		const myScriptUrl = vscode.workspace.getConfiguration("selection-translation").get("GAS setting");
+		const url = `${myScriptUrl}?text=${this.selectString}&source=${this.sourceLanguage}&target=${this.translationLanguage}`;
+		const https = require('https');
+		https.get(url, (res: any) => {
+			//Get the redirect URL
+			let reUrl = res.headers['location']
+			https.get(reUrl, (res: any) => {
+				res.on('data', (chunk: any) => {
+					let outputText = `${chunk}`;
+					this.editor.edit((textEditorEdit) => {
+						textEditorEdit.replace(this.selectRange, outputText);
+					});
+				});
+			});
+		});
+	}
+}
