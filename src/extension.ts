4e68ad31c111861a77614f5391f1cabe08e73862
sakuya <dummy@dummy>
2020-10-04 16:05:10 +0900

    support multi lines

--- src/extension.ts
+++ src/extension.ts
@@ -9,7 +9,6 @@ export function activate(context: vscode.ExtensionContext) {
 		const sourceLangString = vscode.workspace.getConfiguration("selection-translation").get("My language");
 		const translationLang = vscode.workspace.getConfiguration("selection-translation").get("Translation language");
 		let gasTranslation = new GasTranslation(editor, sourceLangString, translationLang)
-		gasTranslation.ReplaceTranslation();
 	});
 	//Translation language => My language
 	let command2 = vscode.commands.registerCommand('selection-translation.fromTranslationToMy', () => {
@@ -18,8 +17,6 @@ export function activate(context: vscode.ExtensionContext) {
 		const sourceLangString = vscode.workspace.getConfiguration("selection-translation").get("Translation language");
 		const translationLang = vscode.workspace.getConfiguration("selection-translation").get("My language");
 		let gasTranslation = new GasTranslation(editor, sourceLangString, translationLang)
-		gasTranslation.ReplaceTranslation();
-
 	});
 	context.subscriptions.push(command1);
 	context.subscriptions.push(command2);
@@ -28,7 +25,6 @@ export function activate(context: vscode.ExtensionContext) {
 class GasTranslation{
 	private editor: vscode.TextEditor;
 	private selectRange: vscode.Range;
-	private selectString: string;
 	private sourceLanguage: string;
 	private translationLanguage: string;
 	constructor(editor:vscode.TextEditor, sourceLang: unknown, translationLang: unknown) {
@@ -47,21 +43,30 @@ class GasTranslation{
 		//Examine the current selected character
 		const startSelectLine: number = this.selectRange.start.line;
 		const EndSelectLine: number = this.selectRange.end.line;
+		let selectString = "";
+		let replaceRange: vscode.Range;
 		if (startSelectLine == EndSelectLine) {
-			this.selectString = this.editor.document.lineAt(startSelectLine).text.slice(this.selectRange.start.character, this.selectRange.end.character);
+			// one line
+			selectString = this.editor.document.lineAt(startSelectLine).text.slice(this.selectRange.start.character, this.selectRange.end.character);
+			this.ReplaceTranslation(selectString, this.selectRange);
 		} else {
-			this.selectString = "";
-			// Note: I want to support multiple lines
-			// selectString = editor.document.lineAt(startSelectLine).text.slice(selectRange.start.character) 
-			// for (let line = startSelectLine + 1; line < EndSelectLine; line++) {
-			// 	selectString = selectString + editor.document.lineAt(line).text;
-			// }
-			// selectString = selectString + editor.document.lineAt(startSelectLine).text.slice(0,selectRange.end.character);
+			//multi lines
+			selectString = editor.document.lineAt(startSelectLine).text.slice(this.selectRange.start.character);
+			replaceRange = new vscode.Range(this.editor.selection.start, editor.document.lineAt(startSelectLine).range.end);
+			this.ReplaceTranslation(selectString, replaceRange);
+			for (let line = startSelectLine + 1; line < EndSelectLine; line++) {
+				selectString = editor.document.lineAt(line).text;
+				replaceRange = editor.document.lineAt(line).range;
+				this.ReplaceTranslation(selectString, replaceRange);
+			}
+			selectString = editor.document.lineAt(EndSelectLine).text.slice(0, this.selectRange.end.character);
+			replaceRange = new vscode.Range(editor.document.lineAt(EndSelectLine).range.start, this.editor.selection.end);
+			this.ReplaceTranslation(selectString, replaceRange);
 		}
 	}
-	ReplaceTranslation() {
+	ReplaceTranslation(SelectString: string, ReplaceRange : vscode.Range) {
 		const myScriptUrl = vscode.workspace.getConfiguration("selection-translation").get("GAS setting");
-		const url = `${myScriptUrl}?text=${this.selectString}&source=${this.sourceLanguage}&target=${this.translationLanguage}`;
+		const url = `${myScriptUrl}?text=${SelectString}&source=${this.sourceLanguage}&target=${this.translationLanguage}`;
 		const https = require('https');
 		https.get(url, (res: any) => {
 			//Get the redirect URL
@@ -70,7 +75,7 @@ class GasTranslation{
 				res.on('data', (chunk: any) => {
 					let outputText = `${chunk}`;
 					this.editor.edit((textEditorEdit) => {
-						textEditorEdit.replace(this.selectRange, outputText);
+						textEditorEdit.replace(ReplaceRange, outputText);
 					});
 				});
 			});
